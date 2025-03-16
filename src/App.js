64712d68e0fe92cf2c72376bbdc5127972dc695b
Amandacorreia1 <amandanascimentocorreia18@gmail.com>
2025-03-16 00:47:13 -0300

    Adjustes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import AppRoutes from './routes/Routes';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { SignUp } from '../components/signUp/signUp.js';
-import { Hello } from '../components/HelloReact.js';
 import Login from '../components/Login';
 import { login } from '../service/auth';
 import MainScreen from '../components/MainScreen';
@@ -41,6 +40,7 @@ const AppRoutes = ({ isAuthenticated, setAuthenticated }) => {
   };
 
   return (
+    <BrowserRouter>
       <Routes>
         {/* Rota de Login */}
         <Route
@@ -58,7 +58,6 @@ const AppRoutes = ({ isAuthenticated, setAuthenticated }) => {
             isAuthenticated ? (
               <Layout setAuthenticated={setAuthenticated}>
                 <MainScreen setAuthenticated={setAuthenticated} />
-                <Hello />
               </Layout>
             ) : (
               <Navigate to="/login" />
@@ -69,6 +68,8 @@ const AppRoutes = ({ isAuthenticated, setAuthenticated }) => {
         {/* Redirecionamento para rotas inválidas */}
         <Route path="*" element={<Navigate to={isAuthenticated ? '/' : '/login'} />} />
       </Routes>
+      </BrowserRouter>
+  
   );
 };
 
