068748b12e06f8dc9ec6b1c727b0285348e5d4b3
Amandacorreia1 <amandanascimentocorreia18@gmail.com>
2025-03-15 23:29:29 -0300

    create router login

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { SignUp } from '../components/signUp/signUp.js';
-import {Hello} from '../components/HelloReact.js'
-
+import { Hello } from '../components/HelloReact.js';
+import Login from '../components/Login';
 
   const AppRoutes = ({ isAuthenticated, setAuthenticated }) => {
     const handleLogin = () => {
@@ -11,6 +11,13 @@ import {Hello} from '../components/HelloReact.js'
 
     return (
       <Routes>
+        {/* Rota de Login */}
+        <Route
+        path="/login"
+        element={!isAuthenticated ? <Login onLogin={handleLogin} /> : <Navigate to="/" />}
+      />
+      
+
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/*"
           element = {
